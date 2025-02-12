d5091440bb534e8f54d41ab3316ee8d54a7d0fa3
tecdevetec <lucashfdantas@gmail.com>
2025-02-12 20:25:34 -0300

    att

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import '/src/components/header/Header.css'
+import '/src/components/global.css'
 
 export default function Header() {
     return (
-        <div>
-            <p>Olá</p>
+        <div className="header">
+            <a href="">Home</a>
+            <a href="">Projetos</a>
+            <a href="">Sobre</a>
+            
         </div>
     )
 }

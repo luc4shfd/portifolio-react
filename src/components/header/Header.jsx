a6d0e04a3d6a36c70b019d96bfff41759d89d9ba
tecdevetec <lucashfdantas@gmail.com>
2025-02-12 21:23:49 -0300

    test img

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '/src/components/header/Header.css'
 import '/src/components/global.css'
+// import lucas from '/src/assets/lucas.jpg'
 
 export default function Header() {
     return (
@@ -9,8 +10,9 @@ export default function Header() {
                 <a href="">Home</a>
                 <a href="">Projetos</a>
                 <a href="">Sobre</a>
+                
            </nav>
-            
+           {/* <img src={lucas} alt="" /> */}
         </div>
     )
 }
